81af39f205bb8696037c7ec17aa84e7998421c82
unknown <cspeck7@gmail.com>
2021-04-20 22:44:19 -0500

    Centering email text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ export default function Home() {
     <div className="h-full flex items-center justify-center bg-black">
       <div className="font-mono text-gray-200">
         <h1 className="font-semibold text-4xl mb-1">Cody Peck</h1>
-        <a href="mailto:cody@codyspeck.com">cody@codyspeck.com</a>
+        <a href="mailto:cody@codyspeck.com" className="block text-center">
+          cody@codyspeck.com
+        </a>
       </div>
     </div>
   );
